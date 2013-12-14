4622dd761b63bdaf6e3eda6f65b5bb3cddacd14d
NickPournaras <garrett.freeborn@gmail.com>
2013-12-14 17:30:55 +0000

    Rotation
    + First iteration of rotation
    + Repositioning of camera and light
    +placed a capsule in level

---
+++ Assets/code/Ship.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour
+{
+	private Vector2 m_v2ShipSpeed = new Vector2( 0.1f, -0.1f );
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		GetInput();
+	}
+
+	// Take input
+	void GetInput()
+	{
+		transform.RotateAround( transform.position, Vector3.forward, Input.GetAxis( "ShipHorizontal" ) );		
+	}
+}
