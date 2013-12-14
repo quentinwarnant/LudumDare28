04289491fe238f5b1cd6dbdfc0d6bfac2efd7bce
snipercommand <thesnipercommand@gmail.Com>
2013-12-14 12:10:25 +0000

    added script to spawn diggedcube

---
+++ Assets/code/Digging.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Digging : MonoBehaviour {
+	
+	public GameObject m_goDiggedCubePrefab;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButtonDown(0))
+		{
+			GameObject.Instantiate(m_goDiggedCubePrefab,transform.position,transform.rotation);  
+		}
+	}
+}
