128287af72e0f8fc50a244ad42c061f48255d278
RickyAbuSiddek <otacon@live.co.uk>
2013-12-14 17:51:51 +0000

    Initial Digger
    Add digger square as digger moves down
    Initial digger movement

---
+++ Assets/code/DiggerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiggerMovement : MonoBehaviour 
+{
+	public float diggerSpeed = 5.0f;
+	private float horizontalMovement;
+	private Vector3 moveDirection = new Vector3(0f,-1f,0f);
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	void Update ()
+	{
+		horizontalMovement = Input.GetAxis("DiggerHorizontal");
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.Translate(moveDirection * diggerSpeed);
+		if (horizontalMovement > 0)
+		{
+			moveDirection += new Vector3(0.1f,0f,0f);
+		}
+		else if (horizontalMovement < 0)
+		{
+			moveDirection += new Vector3(-0.1f,0f,0f);
+		}
+	}
+}
--- Assets/code/Digging.cs
+++ Assets/code/Digging.cs
@@ -1,22 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Digging : MonoBehaviour {
+public class Digging : MonoBehaviour 
+{
 	
 	public GameObject m_goDiggedCubePrefab;
 	
-	
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		GameObject.Instantiate(m_goDiggedCubePrefab,transform.position+new Vector3(0,0.5f,0),transform.rotation);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
 		if(Input.GetMouseButtonDown(0))
 		{
 			GameObject.Instantiate(m_goDiggedCubePrefab,transform.position,transform.rotation);  
+		}
+	}
+	void FixedUpdate()
+	{
+
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("entered: " + other.tag);
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.tag == "DiggedSquare")
+		{
+			GameObject.Instantiate(m_goDiggedCubePrefab,transform.position+new Vector3(0,0.5f,0),transform.rotation);
 		}
 	}
 }
