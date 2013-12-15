64df291d4d0dcff10fc6bfeb14e401965f49f949
RickyAbuSiddek <otacon@live.co.uk>
2013-12-15 12:50:12 +0000

    Trying to test out branches
    We could use branches so that we mess each others code up

--- Assets/code/DiggerMovement.cs
+++ Assets/code/DiggerMovement.cs
@@ -3,32 +3,50 @@ using System.Collections;
 
 public class DiggerMovement : MonoBehaviour 
 {
-	public float m_fDiggerSpeed = 0.01f;
-	private float m_fHorizontalMovement;
-	private Vector3 m_vMoveDirection = new Vector3(0f,-1f,0f);
+	public Vector2 m_v2DiggerSpeed = new Vector2( 0.1f, -0.01f );
+	//private float m_fHorizontalMovement;
+	//private Vector3 m_vMoveDirection = new Vector3(0f,-1f,0f);
 	// Use this for initialization
 	void Start () 
 	{
 	
 	}
-
-	void Update ()
+	// Update is called once per frame
+	void Update()
 	{
-		m_fHorizontalMovement = Input.GetAxis("DiggerHorizontal");
-
+		Move();
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () 
+	// Move in 2D space
+	void Move()
 	{
-		//transform.Translate(m_vMoveDirection * m_fDiggerSpeed);
-		if (m_fHorizontalMovement > 0)
-		{
-			m_vMoveDirection += new Vector3(0.1f,0f,0f);
-		}
-		else if (m_fHorizontalMovement < 0)
-		{
-			m_vMoveDirection += new Vector3(-0.1f,0f,0f);
-		}
+		transform.RotateAround( transform.position, Vector3.forward, GetInput() );
+		transform.Translate( new Vector3( 0, m_v2DiggerSpeed.y, 0 ) );
 	}
+	
+	// Get input
+	float GetInput()
+	{		
+		return Input.GetAxis( "DiggerHorizontal" );
+	}
+
+//	void Update ()
+//	{
+//		m_fHorizontalMovement = Input.GetAxis("DiggerHorizontal");
+//
+//	}
+//	
+//	// Update is called once per frame
+//	void FixedUpdate () 
+//	{
+//		//transform.Translate(m_vMoveDirection * m_fDiggerSpeed);
+//		if (m_fHorizontalMovement > 0)
+//		{
+//			m_vMoveDirection += new Vector3(0.1f,0f,0f);
+//		}
+//		else if (m_fHorizontalMovement < 0)
+//		{
+//			m_vMoveDirection += new Vector3(-0.1f,0f,0f);
+//		}
+//	}
 }
